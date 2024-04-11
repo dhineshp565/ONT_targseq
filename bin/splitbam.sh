78cb69b1f30ac542f8587c311f090b0eb1ff7d91
Dhinesh Periyasamy <64551109+dhineshp565@users.noreply.github.com>
2024-04-11 12:37:10 -0600

    Update splitbam.sh

--- bin/splitbam.sh
+++ bin/splitbam.sh
@@ -9,16 +9,16 @@
 # generate stats prior to read filtering
 samtools view -b -h $2|samtools sort > $1_unfilt.bam
 samtools stats "$1_unfilt.bam" > $1_unfilt_stats.txt
+samtools index "$1_unfilt.bam" > $1_unfilt.bai
+samtools idxstats "$1_unfilt.bam" > $1_unfilt_idxstats.csv
 #generate a  sorted bam file with primary alignments
 samtools view -b -h -F 0x900 -q 30 $2|samtools sort > $1.bam	
 samtools stats "$1.bam" > $1_stats.txt
-#trims primers from both ends of the amplicon using primer bed file
-samtools ampliconclip --both-ends -b $3 "$1.bam" > $1_trimmed.bam
 # sorts the bam file for spitting	
-samtools sort "$1_trimmed.bam" > $1_tr_sorted.bam
+samtools sort "$1.bam" > $1_sorted.bam
 #index sorted bam file and generate read counts for each amplicon
-samtools index "$1_tr_sorted.bam" > $1_sorted.bai
-samtools idxstats "$1_tr_sorted.bam" > $1_idxstats.txt
+samtools index "$1_sorted.bam" > $1_sorted.bai
+samtools idxstats "$1_sorted.bam" > $1_idxstats.txt
 awk '{if ($3 >= 10) print $1,$2,$3}' "$1_idxstats.txt" > $1_mappedreads.txt
 #conditional for empty mapped reads.txt file
 if [ $(wc -l < "$1_mappedreads.txt") -ne 0 ]
@@ -30,19 +30,21 @@ then
 			len=$(echo $lines|cut -f2 -d' ')
 			 # get upper and lower bounds of reference span
 
-      		upper=`echo \$((\${len}+(\${len}*10/100)))`
-      		lower=`echo \$((\${len}-(\${len}*10/100)))`
+	      		upper=`echo $((${len}+(${len}*20/100)))`
+	      		lower=`echo $((${len}-(${len}*20/100)))`
 
 			# split bam 
-			samtools view -b "$1_tr_sorted.bam" "${amp}" > $1_${amp}.bam
+			samtools view -b "$1_sorted.bam" "${amp}" > $1_${amp}.bam
 			# Only reads length with + or - 10% bases is used for consenus
 			samtools view -h "$1_${amp}.bam"|awk -v u=${upper} -v l=${lower} '/^@/|| length($10)>=l && length($10)<=u'|samtools sort > $1_${len}_${amp}.bam
 			# generate stats for near full length reads
 			samtools index "$1_${len}_${amp}.bam" > $1_${len}_${amp}.bai
 			samtools idxstats "$1_${len}_${amp}.bam" > $1_${len}_${amp}_idxstats.txt
-			awk '{if ($3 >= 10) print $1,$2,$3}' "$1_${len}_${amp}_idxstats.txt" > $1_${amp}_mappedreads.txt
+			awk '{print $1,$2,$3}' "$1_${len}_${amp}_idxstats.txt" > $1_${amp}_mappedreads.txt
+			#trims primers from both ends of the amplicon using primer bed file
+			samtools ampliconclip --both-ends -b $3 "$1_${len}_${amp}.bam" > $1_trimmed_${len}_${amp}.bam
 			# generate consensus for full length reads
-			samtools consensus -d 10 -A -l 100 -f fasta "$1_${len}_${amp}.bam" > $1_${amp}.fasta
+			samtools consensus -A -f fasta "$1_trimmed_${len}_${amp}.bam" > $1_${amp}.fasta
 			# change fasta header with sample and amplicon names
 			sed -i "s/>.*/>$1_${amp}_consensus/" $1_${amp}.fasta
 	done < "$1_mappedreads.txt"
@@ -56,3 +58,4 @@ else
 fi
 	# insert headers to mappedreads.txt
 sed -i "1i Amplicon_Name Size $1" "$1_mappedreads.txt"
+sed -i "1i Amplicon_Name Size $1" "$1_full_length_mappedreads.txt"
